e360778f666eb254774fe2a733dd8ebb20674cd2
Rafael Sánchez Córdoba <rafa.tivuron@gmail.com>
2022-09-13 09:27:35 +0200

    why is not working :(

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ThemeContext } from './contexts/theme';
 import './App.css';
 import { Header } from "./components/Header/Header"
 import { About } from "./components/About/About"
-import { Projects } from "./components/Projects/Projects"
+import { Projects }  from "./components/Projects/Projects.jsx"
 import { Skills } from './components/Skills/Skills';
 import { Contact } from './components/Contact/Contact'
 import { ScrollToTop } from './components/ScrollToTop/ScrollToTop';
