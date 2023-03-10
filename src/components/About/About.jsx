46f543e46747a7eb859b40b3b68dbba509d2f006
Rafael Sánchez Córdoba <rafa.tivuron@gmail.com>
2023-03-10 09:38:57 +0100

    #

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ export function About() {
             <p className='about_desc'>I'm Rafa from Spain and I am a dedicated, ambitious, and hardworking individual who is passionate about software development and eager to learn new skills and take on new challenges. I have strong technical skills as well as excellent interpersonal skills, enabling me to work collaboratively with team members and effectively communicate with stakeholders.</p>
 
             <div className='about_contact center'>
-                <a href='../../doc/resume.pdf' download>
+                <a href="/src/components/About/resume.pdf" download>
                     <span type='button' className='btn btn-outline'>
                         Resume
                     </span>
